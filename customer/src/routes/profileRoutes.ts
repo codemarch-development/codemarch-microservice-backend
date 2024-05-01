2fe16d224d5e887075f957ab772b12a6f29e22c1
Anxhid-Mohed <mohammedanshid100@gmail.com>
2024-05-02 01:20:25 +0530

    changed api

--- customer/src/routes/profileRoutes.ts
+++ customer/src/routes/profileRoutes.ts
@@ -11,6 +11,4 @@ router.patch('/edit-profile',authenticateUser,editUser);
 router.delete('/delete-account',authenticateUser,terminateAccount);
 router.patch('/completion/update',authenticateUser,markOrUnmarkCompletion);
 
-router.get('/anshid',authenticateUser,getUser);
-
 export default router ;
